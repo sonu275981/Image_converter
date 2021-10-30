c299820d6ce92a8aa4c8c429b8eb3974f2a9ae20
sonu chaurasia <48214676+sonu275981@users.noreply.github.com>
2021-10-30 20:27:57 +0530

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,4 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win64
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> from tkinter import *   
   
